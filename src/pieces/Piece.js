55372eee74da7a8a0e70546c4f8f9dd0d1ad19db
Jaclyn <jaclyndshahan@gmail.com>
2019-09-29 20:21:05 -0500

    first steps

---
+++ src/pieces/Piece.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class Piece extends Component {
+    constructor(player, iconURL) {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Piece;
