eea0af7ce52d884f0e9f8dd8ba8f475f821026ee
Jaclyn <jaclyndshahan@gmail.com>
2019-09-29 20:38:35 -0500

    setting up piece components

---
+++ src/pieces/Bishop.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Piece from './Piece.js';
+
+class Bishop extends Component {
+    constructor() {
+        super () 
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Bishop;
